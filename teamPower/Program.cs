4eff04fb86999d9903de9a08d52e931553ad1cfd
akauffman01 <akauffman01@bellarmine.edu>
2021-07-21 17:20:26 -0400

    Test Change with GitHub extension

--- teamPower/Program.cs
+++ teamPower/Program.cs
@@ -7,6 +7,7 @@ namespace teamPower
           static void Main(string[] args)
           {
                Console.WriteLine("Hello World!");
+               Console.WriteLine("Here is a change!");
           }
      }
 }
